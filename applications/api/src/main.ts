0fda65ceec8a002398af988e5119050e619c3902
Daniel Hay <haystacker2000@gmail.com>
2024-04-21 22:16:07 +1000

    task(express): Set up express server in API

--- applications/api/src/main.ts
+++ applications/api/src/main.ts
@@ -1,5 +1,11 @@
-export function main() {
-  console.log('Hello world');
-}
+import express, { Express, Request, Response } from "express";
 
-main();
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("API running");
+});
+
+app.listen(port);
+console.log(`API running on http://localhost:${port}`);
